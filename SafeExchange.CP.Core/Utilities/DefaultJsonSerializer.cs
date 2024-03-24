1797048b8ccac886737673c610e7cf4cc6d1d513
yury-opolev <space.oyster@outlook.com>
2024-03-24 12:53:18 +0100

    Minor changes

---
+++ SafeExchange.CP.Core/Utilities/DefaultJsonSerializer.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// DefaultJsonSerializer
+/// </summary>
+
+namespace SafeExchange.CP.Core
+{
+    using System;
+    using System.Text.Json;
+    using System.Text.Json.Serialization;
+
+    public static class DefaultJsonSerializer
+    {
+        private static readonly Lazy<JsonSerializerOptions> JsonOptions = new Lazy<JsonSerializerOptions>(
+            new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase, DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull });
+
+        public static T? Deserialize<T>(string json)
+        {
+            return JsonSerializer.Deserialize<T>(json, options: DefaultJsonSerializer.JsonOptions.Value);
+        }
+
+        public static string Serialize<T>(T value)
+        {
+            return JsonSerializer.Serialize(value, options: DefaultJsonSerializer.JsonOptions.Value);
+        }
+    }
+}

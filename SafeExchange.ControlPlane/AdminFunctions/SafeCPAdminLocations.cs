42c0a3d9da5c93bf3e3efb8d8576b150bc3ca1f6
yury-opolev <space.oyster@outlook.com>
2024-04-01 18:30:29 +0200

    Minor changes

---
+++ SafeExchange.ControlPlane/AdminFunctions/SafeCPAdminLocations.cs
@@ -0,0 +1,40 @@
+﻿/// <summary>
+/// SafeCPAdminLocations
+/// </summary>
+
+namespace SafeExchange.ControlPlane
+{
+    using Microsoft.Azure.Functions.Worker.Http;
+    using Microsoft.Azure.Functions.Worker;
+    using Microsoft.Extensions.Logging;
+    using SafeExchange.CP.Core;
+    using SafeExchange.CP.Core.DatabaseContext;
+    using SafeExchange.CP.Core.Filters;
+    using SafeExchange.CP.Core.Functions.Admin;
+    using SafeExchange.CP.Core.Middleware;
+
+    public class SafeCPAdminLocations
+    {
+        private const string Version = "v1";
+
+        private SafeExchangeCPLocations safeExchangeCPLocationsHandler;
+
+        private readonly ILogger log;
+
+        public SafeCPAdminLocations(SafeExchangeCPDbContext dbContext, ITokenHelper tokenHelper, GlobalFilters globalFilters, ILogger<SafeCPAdminApplications> log)
+        {
+            this.safeExchangeCPLocationsHandler = new SafeExchangeCPLocations(dbContext, tokenHelper, globalFilters);
+            this.log = log ?? throw new ArgumentNullException(nameof(log));
+        }
+
+        [Function("SafeExchangeCP-Location")]
+        public async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", "get", "patch", "delete", Route = $"{Version}/locations/{{locationId}}")]
+            HttpRequestData request,
+            string locationId)
+        {
+            var principal = request.FunctionContext.GetPrincipal();
+            return await this.safeExchangeCPLocationsHandler.Run(request, locationId, principal, this.log);
+        }
+    }
+}
---
+++ SafeExchange.ControlPlane/AdminFunctions/SafeCPAdminLocationsList.cs
@@ -0,0 +1,39 @@
+﻿/// <summary>
+/// SafeCPAdminLocationsList
+/// </summary>
+
+namespace SafeExchange.ControlPlane
+{
+    using Microsoft.Azure.Functions.Worker.Http;
+    using Microsoft.Azure.Functions.Worker;
+    using Microsoft.Extensions.Logging;
+    using SafeExchange.CP.Core;
+    using SafeExchange.CP.Core.DatabaseContext;
+    using SafeExchange.CP.Core.Filters;
+    using SafeExchange.CP.Core.Functions.Admin;
+    using SafeExchange.CP.Core.Middleware;
+
+    public class SafeCPAdminLocationsList
+    {
+        private const string Version = "v1";
+
+        private SafeExchangeCPLocationsList safeExchangeCPLocationsListHandler;
+
+        private readonly ILogger log;
+
+        public SafeCPAdminLocationsList(SafeExchangeCPDbContext dbContext, ITokenHelper tokenHelper, GlobalFilters globalFilters, ILogger<SafeCPAdminApplications> log)
+        {
+            this.safeExchangeCPLocationsListHandler = new SafeExchangeCPLocationsList(dbContext, tokenHelper, globalFilters);
+            this.log = log ?? throw new ArgumentNullException(nameof(log));
+        }
+
+        [Function("SafeExchangeCP-LocationList")]
+        public async Task<HttpResponseData> RunListApplications(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = $"{Version}/locations-list")]
+            HttpRequestData request)
+        {
+            var principal = request.FunctionContext.GetPrincipal();
+            return await this.safeExchangeCPLocationsListHandler.RunList(request, principal, this.log);
+        }
+    }
+}
